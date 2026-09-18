0852d8044a4809f66ccce6e149d6a4076a202132
Ankit <bhartiankit692@gmail.com>
2026-09-19 02:49:59 +0530

    Revamped Profile Stats Page

--- lib/features/profile/providers/profile_providers.dart
+++ lib/features/profile/providers/profile_providers.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../library/providers/library_providers.dart';
+import '../../../core/database/database_provider.dart';
 
 class ProfileStats {
   final int moviesAdded;
   final int showsAdded;
   final int moviesWatched;
   final int showsWatched;
+  final int episodesWatched;
   final int uniqueGenresCount;
   final Map<String, int> watchedGenreCounts;
 
@@ -14,16 +16,24 @@ class ProfileStats {
     required this.showsAdded,
     required this.moviesWatched,
     required this.showsWatched,
+    required this.episodesWatched,
     required this.uniqueGenresCount,
     required this.watchedGenreCounts,
   });
 }
 
+final watchedEpisodesCountProvider = StreamProvider<int>((ref) {
+  final db = ref.watch(databaseProvider);
+  final query = db.select(db.episodes)..where((t) => t.isWatched.equals(true));
+  return query.watch().map((episodes) => episodes.length);
+});
+
 final profileStatsProvider = Provider<AsyncValue<ProfileStats>>((ref) {
   final moviesAsyncValue = ref.watch(savedMoviesProvider);
   final showsAsyncValue = ref.watch(savedShowsProvider);
+  final episodesWatchedAsyncValue = ref.watch(watchedEpisodesCountProvider);
 
-  if (moviesAsyncValue.isLoading || showsAsyncValue.isLoading) {
+  if (moviesAsyncValue.isLoading || showsAsyncValue.isLoading || episodesWatchedAsyncValue.isLoading) {
     return const AsyncValue.loading();
   }
 
@@ -35,8 +45,13 @@ final profileStatsProvider = Provider<AsyncValue<ProfileStats>>((ref) {
     return AsyncValue.error(showsAsyncValue.error!, showsAsyncValue.stackTrace!);
   }
 
+  if (episodesWatchedAsyncValue.hasError) {
+    return AsyncValue.error(episodesWatchedAsyncValue.error!, episodesWatchedAsyncValue.stackTrace!);
+  }
+
   final movies = moviesAsyncValue.value ?? [];
   final shows = showsAsyncValue.value ?? [];
+  final episodesWatchedCount = episodesWatchedAsyncValue.value ?? 0;
 
   final watchedMovies = movies.where((m) => m.status == 'watched').toList();
   final watchedShows = shows.where((s) => s.status == 'watched').toList();
@@ -65,6 +80,7 @@ final profileStatsProvider = Provider<AsyncValue<ProfileStats>>((ref) {
     showsAdded: shows.length,
     moviesWatched: watchedMovies.length,
     showsWatched: watchedShows.length,
+    episodesWatched: episodesWatchedCount,
     uniqueGenresCount: genreCounts.length,
     watchedGenreCounts: genreCounts,
   ));
